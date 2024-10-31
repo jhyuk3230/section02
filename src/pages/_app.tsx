c51f492a15ec34fe8cd0e32bf240834ac3872518
jhyuk3230 <jhyuk3230@naver.com>
2024-10-31 09:21:12 +0900

    테스트커밋

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	const router = useRouter();
+
+	const onClickButton = () => {
+		router.push("/test");
+	}
+
+	useEffect(()=>{
+		router.prefetch('/test');
+	},[])
+
+  return (
+		<>
+			<header className="flex gap-3">
+				<Link href={"/"}>Home</Link>
+				<Link href={"/search"} prefetch={false}>search</Link>
+				<Link href={"/book"}>book</Link>
+			</header>
+			<div>
+				<button onClick={onClickButton}>test</button>
+			</div>
+
+			<Component {...pageProps} />
+		</>
+	)
 }
